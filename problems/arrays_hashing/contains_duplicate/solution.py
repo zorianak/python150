e0826cea183db1913b0e92ead515fa739ef0c5cf
Callie Holmes <github@kimiscool.me>
2026-02-20 12:43:47 -0800

    Write problems 1 and 2

--- problems/arrays_hashing/contains_duplicate/solution.py
+++ problems/arrays_hashing/contains_duplicate/solution.py
@@ -7,4 +7,9 @@ Given an integer array nums, return true if any value appears at least twice in 
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        pass
+        nums_set = set()
+        for num in nums:
+            if num in nums_set:
+                return True
+            nums_set.add(num)
+        return False
--- problems/arrays_hashing/valid_anagram/solution.py
+++ problems/arrays_hashing/valid_anagram/solution.py
@@ -7,4 +7,13 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        charsS, charsT = {}, {}
+        for i in range(len(s)):
+            charsS[s[i]] = charsS.get(s[i], 0) + 1
+            charsT[t[i]] = charsT.get(t[i], 0) + 1
+
+        return charsS == charsT
+            
         pass
